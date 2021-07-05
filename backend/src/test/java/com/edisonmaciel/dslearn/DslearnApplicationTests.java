daa6577d1a380edd32834bda5568dd43551d4b32
macieleds <maciel.eds@gmail.com>
2021-07-04 22:50:28 -0300

    Backend project created

---
+++ backend/src/test/java/com/edisonmaciel/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edisonmaciel.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
