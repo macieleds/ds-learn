360734f79441c878a20a4b5974a85234cc85ff5d
macieleds <maciel.eds@gmail.com>
2021-07-05 00:23:02 -0300

    User, Role

---
+++ backend/src/main/java/com/edisonmaciel/dslearn/entities/Role.java
@@ -0,0 +1,23 @@
+package com.edisonmaciel.dslearn.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+}
