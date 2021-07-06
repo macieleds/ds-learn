352db5f55a8bcecb04ccc1090795121256c3a69e
macieleds <maciel.eds@gmail.com>
2021-07-05 22:38:12 -0300

    Offer

--- backend/src/main/java/com/edisonmaciel/dslearn/entities/Course.java
+++ backend/src/main/java/com/edisonmaciel/dslearn/entities/Course.java
@@ -1,9 +1,20 @@
 package com.edisonmaciel.dslearn.entities;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,4 +32,8 @@ public class Course implements Serializable {
     private String name;
     private String imgUri;
     private String imgGrayUri;
+
+    @OneToMany(mappedBy = "course")
+    private List<Offer> offers = new ArrayList<>();
+
 }
--- backend/src/main/java/com/edisonmaciel/dslearn/entities/Offer.java
+++ backend/src/main/java/com/edisonmaciel/dslearn/entities/Offer.java
@@ -10,8 +10,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.io.Serializable;
+import java.time.Instant;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,13 +22,19 @@ import java.io.Serializable;
 @Setter
 @EqualsAndHashCode
 @Entity
-@Table(name = "tb_role")
-public class Role implements Serializable {
+@Table(name = "tb_offer")
+public class Offer implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String authority;
+    private String edition;
+    private Instant startMoment;
+    private Instant endMoment;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
 
 }
